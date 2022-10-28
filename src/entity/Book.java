9b60cb2e6983cdc2947c2aa7df6f8ffc36abb0d5
pupil <pupil@ivkhk.ee>
2022-10-28 13:47:39 +0300

    new class

---
+++ src/entity/Book.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+/**
+ *
+ * @author pupil
+ */
+public class Book implements Serializable{
+    private String title;
+    private Author[] authors = new Author[0]; 
+
+    public Book() {
+        
+    }
+
+    public Book(String title, Author[] authors) {
+        this.title = title;
+        this.authors = authors;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Author[] authors) {
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{"
+                + "title=" + title 
+                + ", authors=" + Arrays.toString(authors)
+                + '}';
+    }
+
+    public void addAuthor(Author author) {
+        Author[] newAuthors = Arrays.copyOf(authors, authors.length+1);
+        newAuthors[newAuthors.length-1] = author;
+        this.authors = newAuthors;
+    }
+    
+    public void removeAuthor(int numberOfAuthor){
+        //обнуляем указанного автора (по индексу)
+        this.getAuthors()[numberOfAuthor-1]=null;
+        //создаем массив с количеством элементов на 1 меньше
+        Author[] newAuthors = new Author[this.getAuthors().length-1];
+        // в цикле копируем элементы в новый массив не учитывая обнуленную ячейку
+        int j = 0;
+        for (Author author : this.getAuthors()) {
+            if (author != null) {
+                newAuthors[j] = author;
+                j++;
+            }
+        }
+        //копируем ссылку на новый массив в книгу
+        this.setAuthors(newAuthors);
+    }
+    
+}
---
+++ src/kseniiaodintsovaivkhk_library/App.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kseniiaodintsovaivkhk_library;
+
+import entity.Author;
+import entity.Book;
+import entity.History;
+import entity.Reader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import managers.BookManager;
+import managers.HistoryManager;
+import managers.ReaderManager;
+/**
+ *
+ * @author pupil
+ */
+public class App {
+    private final Scanner scanner;
+    private final BookManager bookManager;
+    private final ReaderManager readerManager;
+    private final HistoryManager historyManager;
+    private Book[] books;
+    private Reader[] readers;
+    private History[] histories;
+
+    public App() {
+        scanner = new Scanner(System.in);
+        bookManager = new BookManager();
+        readerManager = new ReaderManager();
+        historyManager = new HistoryManager();
+        
+        books = new Book[0];
+        readers = new Reader[0];
+        histories = new History[0];
+        testAddBook();
+        testAddReader();
+    }
+    
+    public void run(){
+        boolean repeat = true;
+        do{
+            System.out.println("Функции приложения:");
+            System.out.println("0. Закрыть приложение");
+            System.out.println("1. Добавить книгу");
+            System.out.println("2. Добавить читателя");
+            System.out.println("3. Выдать книгу");
+            System.out.println("4. Вернуть книгу");
+            System.out.println("5. Список книг");
+            System.out.println("6. Список читателей");
+            System.out.println("7. Список выданных книг");
+            System.out.println("8. Изменить данные читателя");
+            System.out.println("9. Редактирование книги");
+            System.out.print("Выберите номер функции: ");
+            int task = scanner.nextInt();
+            scanner.nextLine();
+            switch (task) {
+                case 0:
+                    System.out.println("0. Закрыть приложение");
+                    repeat = false;
+                    break;
+                case 1:
+                    System.out.println("Выбрана задача: 1. Добавить книгу");
+                    addBook(bookManager.createBook());
+                try {
+                    FileOutputStream fileOutputStream = new FileOutputStream("MyBook");
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                    objectOutputStream.writeObject(books);
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, "Нет такого файла", ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, "Ошибка ввода / вывода", ex);
+                }
+            
+                    break;
+                case 2:
+                    System.out.println("2. Добавить читателя");
+                    addReader(readerManager.createReader());
+                    break;
+                case 3:
+                    System.out.println("3. Выдать книгу");
+                    addHistories(historyManager.takeOnBook(readers,books));
+                    break;
+                case 4:
+                    System.out.println("4. Вернуть книгу");
+                    histories = historyManager.returnBook(histories);
+                    break;
+                case 5:
+                    System.out.println("5. Список книг");
+                    bookManager.printListBooks(books);
+                    break;
+                case 6:
+                    System.out.println("6. Список читателей");
+                    readerManager.printListReaders(readers);
+                    break;
+                case 7:
+                    System.out.println("7. Список выданных книг");
+                    historyManager.printListReadingBooks(histories);
+                    break;
+                case 8:
+                    System.out.println("8. Изменить данные читателя");
+                    readers = readerManager.changeReader(readers);
+                    break;
+                case 9:
+                    System.out.println("9. Редактирование книги");
+                    books = bookManager.changeBook(books);
+                    break;
+                default:
+                    System.out.println("Выберите номер функции из списка!");
+            }
+        }while(repeat);
+        System.out.println("Пока!");
+    }
+
+    private void addBook(Book book){
+        books = Arrays.copyOf(books, books.length+1);
+        books[books.length-1] = book;
+    }
+    
+    private void addReader(Reader reader) {
+        readers = Arrays.copyOf(readers, readers.length + 1);
+        readers[readers.length - 1] = reader;
+    }
+    private void addHistories(History history) {
+        histories = Arrays.copyOf(histories, histories.length + 1);
+        histories[histories.length - 1] = history;
+    }
+    
+    private void testAddBook(){
+        Book book = new Book();
+        book.setTitle("Voina i mir");
+        Author author = new Author("Lev", "Tolstoy");
+        book.addAuthor(author);
+        this.books = Arrays.copyOf(this.books, this.books.length + 1);
+        this.books[this.books.length - 1] = book;
+    }
+    private void testAddReader(){
+        Reader reader = new Reader("Ivan", "Ivanov","56564567");
+        readers = Arrays.copyOf(readers, readers.length + 1);
+        readers[readers.length - 1] = reader;
+    }
+}
